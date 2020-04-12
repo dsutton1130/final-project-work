2b2cec96327e697e6133a908711b718a330ab69f
David Sutton <dsutton@g.harvard.edu>
2020-04-12 16:43:03 -0400

    fixed publishing issue

--- shiny_app/final_proj_shiny/app.R
+++ shiny_app/final_proj_shiny/app.R
@@ -58,22 +58,100 @@ server <- function(input, output, session){
     
     output$switchers <- renderPlot({
         if(input$plot1 == "race_12"){
-            s1
+          switchers %>%
+            filter(p12switch == 1) %>%
+            ggplot(aes(race_12, ..prop.., fill = reorder(presvote12, race_12)), stat = "count") +
+            geom_bar(aes(y = ..count../sum(..count..))) + scale_y_continuous(labels = scales::percent) +
+            xlab("Race of Switchers") + ylab("Percentage of Switchers") +
+            ggtitle("President 2012 Switchers by Race") + scale_x_continuous(breaks = c(1, 2, 3, 4, 5, 6, 7),
+                                                                             label = c("White", "Black",
+                                                                                       "Hispanic", "Asian",
+                                                                                       "Native American",
+                                                                                       "Mixed", "Other")) +
+            theme(panel.background = element_rect("white"), axis.line.x.bottom = element_line("black"),
+                  axis.line.y.left = element_line("black")) + scale_fill_manual("Voted for",
+                                                                                labels = c("Obama-Romney",
+                                                                                           "McCain-Obama"),
+                                                                                values=c("red", "blue"))
             
         } else if(input$plot1 == "gender_12"){
-            s2
+          switchers %>%
+            filter(p12switch == 1) %>%
+            ggplot(aes(gender_12, ..prop.., fill = reorder(presvote12, gender_12)), stat = "count") +
+            geom_bar(aes(y = ..count../sum(..count..))) + scale_y_continuous(labels = scales::percent) +
+            xlab("Gender of Switchers") + ylab("Percentage of Switchers") +
+            ggtitle("President 2012 Switchers by Gender") + scale_x_continuous(breaks = c(1, 2),
+                                                                               label = c("Male", "Female")) +
+            theme(panel.background = element_rect("white"), axis.line.x.bottom = element_line("black"),
+                  axis.line.y.left = element_line("black")) + scale_fill_manual("Voted for",
+                                                                                labels = c("Romney", "Obama"),
+                                                                                values=c("red", "blue"))
             
         } else if(input$plot1 == "pres.agegroup12"){
-            s3
+          switchers %>%
+            filter(p12switch == 1) %>%
+            ggplot(aes(pres.agegroup12, ..prop.., fill = reorder(presvote12, pres.agegroup12)), stat = "count") +
+            geom_bar(aes(y = ..count../sum(..count..))) + xlab("Age of Switchers") +
+            ylab("Percentage of Switchers") + ggtitle("President 2012 Switchers by Age") +
+            scale_x_continuous(breaks = c(1, 2, 3, 4, 5, 6, 7, 8), label = c("18-24", "25-34", "35-44",
+                                                                             "45-54", "55-64", "65-74", "75-84",
+                                                                             "85-94")) +
+            scale_y_continuous(labels = scales::percent) + theme(panel.background = element_rect("white"),
+                                                                 axis.line.x.bottom = element_line("black"),
+                                                                 axis.line.y.left = element_line("black")) +
+            scale_fill_manual("Voted for", labels = c("Romney", "Obama"), values=c("red", "blue"))
             
         } else if(input$plot1 == "educ_12"){
-            s4
+          switchers %>%
+            filter(p12switch == 1) %>%
+            ggplot(aes(educ_12, ..prop.., fill = reorder(presvote12, educ_12)), stat = "count") +
+            geom_bar(aes(y = ..count../sum(..count..))) + scale_y_continuous(labels = scales::percent) +
+            xlab("Education Level") + ylab("Percentage of Switchers") +
+            ggtitle("President 2012 Switchers by Education") + scale_x_continuous(breaks = c(1, 2, 3, 4, 5, 6),
+                                                                                  label = c("No High School",
+                                                                                            "High school graduate",
+                                                                                            "Some college", "2-year",
+                                                                                            "4-year", "Post-grad")) +
+            theme(panel.background = element_rect("white"), axis.line.x.bottom = element_line("black"),
+                  axis.line.y.left = element_line("black")) + scale_fill_manual("Voted for",
+                                                                                labels = c("Romney", "Obama"),
+                                                                                values=c("red", "blue")) +
+            coord_flip()
             
         } else if(input$plot1 == "partyreg12"){
-            s5
+          switchers %>%
+            filter(p12switch == 1, partyreg12 < 5) %>%
+            ggplot(aes(partyreg12, ..prop.., fill = reorder(presvote12, partyreg12)), stat = "count") +
+            geom_bar(aes(y = ..count../sum(..count..))) + scale_y_continuous(labels = scales::percent) +
+            xlab("Party of Switchers") + ylab("Percentage of Switchers") +
+            ggtitle("President 2012 Switchers by Party") + scale_x_continuous(breaks = c(1, 2, 3, 4),
+                                                                              label = c("None/Ind/Didn't Say",
+                                                                                        "Democratic", "Republican",
+                                                                                        "Other")) +
+            theme(panel.background = element_rect("white"), axis.line.x.bottom = element_line("black"),
+                  axis.line.y.left = element_line("black")) + scale_fill_manual("Voted for",
+                                                                                labels = c("Romney", "Obama"),
+                                                                                values=c("red", "blue"))
             
         } else if(input$plot1 == "income.12"){
-            s6
+          switchers %>%
+            filter(p12switch == 1) %>%
+            ggplot(aes(income.12, ..prop.., fill = reorder(presvote12, income.12)), stat = "count") +
+            geom_bar(aes(y = ..count../sum(..count..))) + scale_y_continuous(labels = scales::percent) +
+            xlab("Income of Switchers") + ylab("Percentage of Switchers") +
+            ggtitle("President 2012 Switchers by Income") + scale_x_continuous(breaks = c(1, 2, 3, 4, 5, 6),
+                                                                               label = c("0-29,999",
+                                                                                         "30,000-59,999",
+                                                                                         "60,000-99,999",
+                                                                                         "100,000-149,999",
+                                                                                         "150,000+",
+                                                                                         "Didn't say/Skipped/Not asked")) +
+            theme(panel.background = element_rect("white"), axis.line.x.bottom = element_line("black"),
+                  axis.line.y.left = element_line("black")) + scale_fill_manual("Voted for",
+                                                                                labels = c("Obama-Romney",
+                                                                                           "McCain-Obama"),
+                                                                                values=c("red", "blue")) +
+            coord_flip()
         }
         
         
